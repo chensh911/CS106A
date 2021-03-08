5e3e258afafe08c09fd71565cdc73a8968c4b84b
ChenShangheng <3500161@qq.com>
2021-03-08 17:39:35 +0800

    hw1 midpoint

--- CS106A-Assignment1/src/MidpointFinderKarel.java
+++ CS106A-Assignment1/src/MidpointFinderKarel.java
@@ -1,11 +1,71 @@
 /*
- * TODO: comment this program
+ * This is the last question for this assignment
+ * @author Shangheng Chen
  */
 
 import stanford.karel.*;
 
 public class MidpointFinderKarel extends SuperKarel {
 	
-	// TODO: write the code to implement this program
+    public void run() {
+        putBeeper();
+        while(true) {
+            if (!putRight()) {
+                break;
+            }
+            if (!putLeft()) {
+                break;
+            }
+        }
+    }
 
+    public boolean putRight() {
+        if (frontIsBlocked()) {
+            turnLeft(); // final: up
+            return false;
+        }
+        move();
+        if (beepersPresent()) {
+            pickBeeper();
+            turnLeft();
+            turnLeft();
+            move();
+            turnRight(); // final: up
+            return false;
+        } else {
+            while(beepersPresent() | frontIsClear()) {
+                if (beepersPresent()) {
+                    pickBeeper();
+                    turnLeft();
+                    turnLeft();
+                    move();
+                    turnRight();
+                    turnRight(); // right
+                    break;
+                }
+                move();
+            }
+            putBeeper();
+            turnLeft();
+            turnLeft(); // left
+        }
+        return true;
+    }
+
+    public boolean putLeft() {
+        move();
+        if (beepersPresent()) {
+            turnRight(); // final: up
+            return false;
+        }
+        while (!beepersPresent()) {
+            move();
+        }
+        pickBeeper();
+        turnLeft();
+        turnLeft();
+        move();
+        putBeeper();
+        return true;
+    }
 }
